a9bda96456b731aac8508b09e62d8f0ace287e59
erinawidjaja <erina.widjaja@gmail.com>
2018-07-16 18:56:27 +0000

    automatically backed up by learn

--- index.rb
+++ index.rb
@@ -38,5 +38,26 @@ cities = {
  }
 
  def city_populations(cities)
-#   your code here!
+cities = {
+:new_york => { 
+     :mayor => "Bill DeBlasio",
+     :population => 8337000,
+     :website => "http://www.nyc.gov"
+   },
+   :los_angeles => { 
+     :mayor => "Eric Garcetti",
+     :population => 3884307,
+     :website => "http://www.lacity.org"
+   },
+   :miami => { 
+     :mayor => "Tomás Regalado",
+     :population => 419777,
+     :website => "http://www.miamigov.com"
+   },
+  :chicago => { 
+     :mayor => "Rahm Emanuel",
+     :population => 2695598,
+    :website => "http://www.cityofchicago.org/"
+   }
+ }
  end
